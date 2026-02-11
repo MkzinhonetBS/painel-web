bc8cedcbf48b6f086dc4111eee78eb520d5a445a
MKZinhho <danonediscord055@gmail.com>
2026-02-11 01:39:22 -0300

    fix static path render

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const db = require('./db');
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // ================= EJS =================
 app.set('view engine', 'ejs');
